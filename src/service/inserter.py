58a76bbfa69d9a368adb363ca19c82f6fd7d8048
ptrLx <62720738+ptrLx@users.noreply.github.com>
2023-10-06 18:38:05 +0200

    fix(inserter): do not search in subfolders

--- src/service/inserter.py
+++ src/service/inserter.py
@@ -3,25 +3,32 @@ import os
 from util.datereader import read_date_and_type
 from model.image_entry import ImageEntry
 from util.config import image_extensions
+from os import listdir
+from os.path import isfile, join
 
 
 def inserter_service(controller):
     folder = controller.args.get_image_folder_path()
-    for _root, _dirs, files in os.walk(folder):
-        for file_name in files:
-            if file_name.lower().endswith(image_extensions):
-                date, type = read_date_and_type(folder, file_name)
 
-                if not type:
-                    logging.warning(f"Date of image {file_name} not readable.")
-                    controller.counts["error"] += 1
-                    return
+    # * also search in subfolders
+    # // for _root, _dirs, files in os.walk(folder):
+    # //    for file_name in files:
 
-                image_entry = ImageEntry(file_name, date, type)
-                date_number = image_entry.date_number()
+    files = [f for f in listdir(folder) if isfile(join(folder, f))]
+    for file_name in files:
+        if file_name.lower().endswith(image_extensions):
+            date, type = read_date_and_type(folder, file_name)
 
-                logging.debug(f"Date {date} found for image '{file_name}' ({type}).")
-                controller.images[date_number].append(image_entry)
+            if not type:
+                logging.warning(f"Date of image {file_name} not readable.")
+                controller.counts["error"] += 1
+                return
+
+            image_entry = ImageEntry(file_name, date, type)
+            date_number = image_entry.date_number()
+
+            logging.debug(f"Date {date} found for image '{file_name}' ({type}).")
+            controller.images[date_number].append(image_entry)
 
     controller.set_event("insert_finished")
 
